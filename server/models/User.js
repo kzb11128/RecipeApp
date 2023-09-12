56c185d9f298390126de97101a976b2e075d2530
Zhang <kzb11128@gmail.com>
2023-09-12 19:18:53 -0400

    remove username from User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,9 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-  username: {
+  name: {
     type: String,
     required: true,
-    unique: true,
-    trim: true,
   },
 
   email: {
@@ -13,6 +11,7 @@ const userSchema = new Schema({
     required: true,
     unique: true,
     match: [/.+@.+\..+/, 'Must match an email address!'],
+    trim: true,
   },
 
   password: {
