d375d84f8db2fe79c6c694c7e97772bf14c48b6f
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-09-18 18:46:23 -0400

    Put a login component in the right spot

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,63 +1,9 @@
-import React from 'react';
-
-export default function Login() {
-  
-    return (
-        <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-            <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-                <h1 className="text-3xl font-semibold text-center text-green-700 underline">
-                   Sign in
-                </h1>
-                <form className="mt-6">
-                    <div className="mb-2">
-                        <label
-                            for="username"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
-                            Username
-                        </label>
-                        <input
-                            type="text"
-                            id="username"
-                            className="block w-full px-4 py-2 mt-2 text-green-700 bg-white border rounded-md focus:border-green-400 focus:ring-green-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                        />
-                    </div>
-                    <div className="mb-2">
-                        <label
-                            for="password"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            className="block w-full px-4 py-2 mt-2 text-green-700 bg-white border rounded-md focus:border-green-400 focus:ring-green-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                        />
-                    </div>
-                    <a
-                        href="#"
-                        className="text-xs text-green-600 hover:underline"
-                    >
-                        Forget Password?
-                    </a>
-                    <div className="mt-6">
-                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-green-700 rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600">
-                            Login
-                        </button>
-                    </div>
-                </form>
-
-                <p className="mt-8 text-xs font-light text-center text-gray-700">
-                    {" "}
-                    Don't have an account?{" "}
-                    <a
-                        href="#"
-                        className="font-medium text-green-600 hover:underline"
-                    >
-                        Sign up
-                    </a>
-                </p>
-            </div>
-        </div>
-    );
+import Login from "./components/Login/index.js"
+import React from "react";
+export default function App() {
+  return (
+    <main>
+      <Login />
+    </main>
+  );
 }
